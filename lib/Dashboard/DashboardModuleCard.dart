4e45bc03b64422da0b65c7ed7438d25032026120
Nausheen-Khanam <nausheenkhanam34@gmail.com>
2023-05-17 21:25:58 +0530

    card changes

--- lib/Dashboard/DashboardModuleCard.dart
+++ lib/Dashboard/DashboardModuleCard.dart
@@ -21,7 +21,7 @@ class DashboardModuleCard extends StatelessWidget {
           padding: EdgeInsets.all(16.0),
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              colors: [Colors.green.shade200, Colors.teal.shade300],
+              colors: [Colors.pinkAccent.shade200, Colors.teal.shade300],
               begin: Alignment.topLeft,
               end: Alignment.bottomRight,
             ),
