0ece2e6e1ee3037ab1d028ed7125c574814e31d0
ahmedrazakhan-412 <ahmedrazakhan412@gmail.com>
2023-05-18 14:45:31 +0530

    PharmEasy code 7

--- lib/Login/LoginScreen.dart
+++ lib/Login/LoginScreen.dart
@@ -1,69 +1,71 @@
 import 'package:flutter/material.dart';
-import 'RegistrationScreen.dart';
 import 'package:medicalstore/Dashboard/DashboardScreen.dart';
-
 
 class LoginScreen extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('User Login'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/images/logo.jpeg'), 
-            SizedBox(height: 20.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Email',
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title:Center(
+          child: Text('User Login'),
+    ),
+    ),
+    body: Container(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Image.asset('assets/images/login.jpeg'),
               ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 10.0),
-            TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20.0),
-            ElevatedButton(
-              onPressed: () {
-                // Perform user login logic
-
-                // Simulate successful login
-                bool isLoggedIn = true;
-
-                if (isLoggedIn) {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => DashboardScreen(),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
                     ),
-                  );
-                }
-              },
-              child: Text('Login'),
-            ),
-            SizedBox(height: 10.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RegistrationScreen(),
-                  ),
-                );
-              },
-              child: Text('Don\'t have an account? Register'),
-            ),
-          ],
-        ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                      ),
+                      obscureText: true,
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Perform user login logic
+
+                        // Simulate successful login
+                        bool isLoggedIn = true;
+
+                        if (isLoggedIn) {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => DashboardScreen(),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text('Login'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
