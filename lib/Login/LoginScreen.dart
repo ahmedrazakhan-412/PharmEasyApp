ada9019efcffe8ec78ffb09c53c4a3cd033a7715
Nausheen-Khanam <nausheenkhanam34@gmail.com>
2023-05-19 17:33:02 +0530

    card changes Login Changes

--- lib/Login/LoginScreen.dart
+++ lib/Login/LoginScreen.dart
@@ -34,113 +34,116 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Center(
-          child: Text('User Login'),
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30),
+            child: Text('User Login'),
+          ),
         ),
       ),
       body: Center(
-        child: Container(
-          padding: EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      height: 300,
-                      width: 300,
-                      child: Image.asset('assets/images/login.jpg'),
-                    ),
-                  ],
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 200,
+                    width: 300,
+                    child: Image.asset('assets/images/login.jpg'),
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 40,
-                width: 250,
-                child: TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    hintText: 'Username/E-mail',
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                    prefixIcon: Icon(Icons.person),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 0.25),
-                    ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 40,
+              width: 300,
+              child: TextField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  hintText: 'Username/E-mail',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  prefixIcon: Icon(Icons.person),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 0.25),
                   ),
                 ),
               ),
-              SizedBox(height: 10.0),
-              SizedBox(
-                height: 40,
-                width: 250,
-                child: TextField(
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    contentPadding:
-                        EdgeInsets.symmetric(horizontal: 5, vertical: 8),
-                    prefixIcon: Icon(Icons.lock),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(width: 0.25),
-                    ),
+            ),
+            SizedBox(height: 10.0),
+            SizedBox(
+              height: 40,
+              width: 300,
+              child: TextField(
+                controller: passwordController,
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  contentPadding:
+                      EdgeInsets.symmetric(horizontal: 5, vertical: 8),
+                  prefixIcon: Icon(Icons.lock),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(width: 0.25),
                   ),
-                  obscureText: true,
                 ),
+                obscureText: true,
               ),
-              SizedBox(height: 20.0),
-              ElevatedButton(
-                onPressed: () async {
-                  // Perform user login logic
-                  String email = emailController.text; // Retrieve email from the text field
-                  String password = passwordController.text; // Retrieve password from the text field
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: () async {
+                // Perform user login logic
+                String email = emailController.text; // Retrieve email from the text field
+                String password = passwordController.text; // Retrieve password from the text field
 
-                  // Fetch user registration data from the database
-                  List<Map<String, dynamic>> userRegistrationData =
-                      await retrieveUserRegistrationData();
+                // Fetch user registration data from the database
+                List<Map<String, dynamic>> userRegistrationData =
+                    await retrieveUserRegistrationData();
 
-                  // Check if the email and password match
-                  bool isLoggedIn = userRegistrationData.any((userData) =>
-                      userData['email'] == email &&
-                      userData['password'] == password);
+                // Check if the email and password match
+                bool isLoggedIn = userRegistrationData.any((userData) =>
+                    userData['email'] == email &&
+                    userData['password'] == password);
 
-                  if (isLoggedIn) {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => DashboardScreen(),
-                      ),
-                    );
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          title: Text('Login Failed'),
-                          content: Text(
-                            'Invalid email or password.',
-                            style: TextStyle(color: Colors.red), // Set the text color to red
+                if (isLoggedIn) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DashboardScreen(),
+                    ),
+                  );
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Login Failed'),
+                        content: Text(
+                          'Invalid email or password.',
+                          style: TextStyle(color: Colors.red), // Set the text color to red
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
                           ),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('OK'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                },
-                child: Text('Login'),
-              ),
-            ],
-          ),
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+              child: Text('Login'),
+            ),
+          ],
         ),
       ),
     );
